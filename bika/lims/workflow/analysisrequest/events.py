02bb1cdb424c68ee3bb458d503af291f1c15db36
Jordi Puiggené <jpuiggene@naralabs.com>
2017-07-17 15:21:46 +0200

    Sample-related events revisited

--- bika/lims/workflow/analysisrequest/events.py
+++ bika/lims/workflow/analysisrequest/events.py
@@ -1,138 +1,230 @@
-from Products.CMFCore.utils import getToolByName
+# coding=utf-8
 from DateTime import DateTime
-
-from bika.lims import logger
 from bika.lims.workflow import doActionFor
 from bika.lims.workflow import getCurrentState
-from bika.lims.workflow import isBasicTransitionAllowed
 
 
-def _promote_transition(obj, transition_id):
-    """Promotes the transition passed in to the object's parent
-    :param obj: Analysis Request for which the transition has to be promoted
+def _promote_transition(analysis_request, transition_id):
+    """Promotes the transition passed in to the parent of the Sample passed in.
+    If the Analysis Request passed in has no Sample assigned, does nothing.
+
+    :param analysis_request: Analysis Request that promotes the transition
     :param transition_id: Unique id of the transition
     """
-    sample = obj.getSample()
+    sample = analysis_request.getSample()
     if sample:
         doActionFor(sample, transition_id)
 
 
-def after_no_sampling_workflow(obj):
-    """Method triggered after a 'no_sampling_workflow' transition for the
-    Analysis Request passed inis performed. Performs the same transition to the
-    parent's Sample.
-    This function is called automatically by
-    bika.lims.workflow.AfterTransitionEventHandler
-    :param obj: Analysis Request affected by the transition
-    :type obj: AnalysisRequest
-    """
-
-    # Do note that the 'no_sampling' transition for Sample already transitions
-    # all the analyses associated to all the sample partitions, so there
-    # is no need to transition neither the analyses nor partitions here
-    _promote_transition(obj, 'no_sampling_workflow')
-
-
-def after_sampling_workflow(obj):
+def after_no_sampling_workflow(analysis_request):
     """Method triggered after a 'sampling_workflow' transition for the
-    Analysis Request passed in is performed. Performs the same transition to
-    the parent's Sample.
+    Analysis Request passed in is performed. Tries to perform the same
+    transition to the Sample associated to the analysis request passed in.
+
     This function is called automatically by
     bika.lims.workflow.AfterTransitionEventHandler
-    :param obj: Analysis Request affected by the transition
-    :type obj: AnalysisRequest
+
+    :param analysis_request: Analysis Request affected by the transition
+    :type analysis_request: AnalysisRequest
     """
-
-    # Do note that the 'sampling' transition for Sample already transitions
-    # all the analyses associated to all the sample partitions, so there
-    # is no need to transition neither the analyses nor partitions here
-    _promote_transition(obj, 'sampling_workflow')
+    _promote_transition(analysis_request, 'no_sampling_workflow')
 
 
-def after_preserve(obj):
+def after_sampling_workflow(analysis_request):
+    """Method triggered after a 'sampling_workflow' transition for the
+    Analysis Request passed in is performed. Tries to perform the same
+    transition to the Sample associated to the analysis request passed in.
+
+    This function is called automatically by
+    bika.lims.workflow.AfterTransitionEventHandler
+
+    :param analysis_request: Analysis Request affected by the transition
+    :type analysis_request: AnalysisRequest
+    """
+    _promote_transition(analysis_request, 'sampling_workflow')
+
+
+def after_to_be_preserved(analysis_request):
+    """Method triggered after a 'sampling_workflow' transition for the
+    Analysis Request passed in is performed. Tries to perform the same
+    transition to the Sample associated to the analysis request passed in.
+
+    This function is called automatically by
+    bika.lims.workflow.AfterTransitionEventHandler
+
+    :param analysis_request: Analysis Request affected by the transition
+    :type analysis_request: AnalysisRequest
+    """
+    _promote_transition(analysis_request, 'to_be_preserved')
+
+
+def after_preserve(analysis_request):
     """Method triggered after a 'preserve' transition for the Analysis Request
-    passed in is performed. Promotes the same transition to parent's Sample.
+    passed in is performed. Tries to perform the same transition to the Sample
+    associated to the analysis request passed in.
+
     This function is called automatically by
     bika.lims.workflow.AfterTransitionEventHandler
-    :param obj: Analysis Request affected by the transition
-    :type obj: AnalysisRequest
+
+    :param analysis_request: Analysis Request affected by the transition
+    :type analysis_request: AnalysisRequest
     """
-
-    # Do note that the 'preserve' transition for Sample already transitions
-    # all the analyses associated to all the sample partitions, so there
-    # is no need to transition neither the analyses nor partitions here
-    _promote_transition(obj, 'preserve')
+    _promote_transition(analysis_request, 'preserve')
 
 
-def after_schedule_sampling(obj):
-    """Method triggered after a 'schedule_sampling' transition for the Analysis
-    Request passed in is performed. Promotes the same transition to parent's
-    Sample.
+def after_schedule_sampling(analysis_request):
+    """Method triggered after a 'schedule_sampling' transition for the
+    Analysis Request passed in is performed. Tries to perform the same
+    transition to the Sample associated to the analysis request passed in.
+
     This function is called automatically by
     bika.lims.workflow.AfterTransitionEventHandler
-    :param obj: Analysis Request affected by the transition
-    :type obj: AnalysisRequest
+
+    :param analysis_request: Analysis Request affected by the transition
+    :type analysis_request: AnalysisRequest
     """
-    _promote_transition(obj, 'schedule_sampling')
+    _promote_transition(analysis_request, 'schedule_sampling')
 
 
-def after_sample(obj):
+def after_sample(analysis_request):
     """Method triggered after a 'sample' transition for the Analysis Request
-    passed in is performed. Promotes sample transition to parent's sample
-    bika.lims.workflow.AfterTransitionEventHandler
-    :param obj: Analysis Request affected by the transition
-    :type obj: AnalysisRequest
-    """
-    # Do note that the 'sample' transition for Sample already transitions
-    # all the analyses associated to all the sample partitions, so there
-    # is no need to transition neither the analyses nor partitions here
-    _promote_transition(obj, 'sample')
+    passed in is performed. Tries to perform the same transition to the Sample
+    associated to the analysis request passed in.
 
-
-def after_receive(obj):
-    """Method triggered after a 'receive' transition for the Analysis Request
-    passed in is performed. Responsible of triggering cascade actions such as
-    transitioning the container (sample), as well as associated analyses.
     This function is called automatically by
     bika.lims.workflow.AfterTransitionEventHandler
-    :param obj: Analysis Request affected by the transition
-    :type obj: AnalysisRequest
+
+    :param analysis_request: Analysis Request affected by the transition
+    :type analysis_request: AnalysisRequest
     """
-    obj.setDateReceived(DateTime())
-    obj.reindexObject(idxs=["getDateReceived", ])
-
-    # Do note that the 'sample' transition for Sample already transitions
-    # all the analyses associated to all the sample partitions, so there
-    # is no need to transition neither the analyses nor partitions here
-    _promote_transition(obj, 'receive')
+    _promote_transition(analysis_request, 'sample')
 
 
-def after_reject(obj):
+def after_sample_due(analysis_request):
+    """Method triggered after a 'sample_due' transition for the Analysis Request
+    passed in is performed. Tries to perform the same transition to the Sample
+    associated to the analysis request passed in.
+
+    This function is called automatically by
+    bika.lims.workflow.AfterTransitionEventHandler
+
+    :param analysis_request: Analysis Request affected by the transition
+    :type analysis_request: AnalysisRequest
+    """
+    _promote_transition(analysis_request, 'sample_due')
+
+
+def after_receive(analysis_request):
+    """Method triggered after a 'preserve' transition for the Analysis Request
+    passed in is performed.
+
+    Stores the value for "Date Received" field and also tries to perform the
+    same transition to the Sample associated to the analysis request passed in
+
+    This function is called automatically by
+    bika.lims.workflow.AfterTransitionEventHandler
+
+    :param analysis_request: Analysis Request affected by the transition
+    :type analysis_request: AnalysisRequest
+    """
+
+    # In most cases, the date of a given transition can be retrieved from an
+    # object by using a getter that delegates the action to getTransitionDate,
+    # without the need of storing the value manually.
+    # This is a different case: a DateTimeField/Widget field is explicitly
+    # declared in AR's schema because in some cases, the user may want to
+    # change the Received Date by him/herself. For this reason, we set the
+    # value manually here.
+    analysis_request.setDateReceived(DateTime())
+    analysis_request.reindexObject(idxs=["getDateReceived", ])
+    _promote_transition(analysis_request, 'receive')
+
+
+def after_reject(analysis_request):
     """Method triggered after a 'reject' transition for the Analysis Request
-    passed in is performed. Transitions and sets the rejection reasons to the
-    parent Sample. Also transitions the analyses assigned to the AR
+    passed in is performed.
+
+    Transitions and sets the rejection reasons to the parent Sample. Also
+    transitions the analyses assigned to the AR
+
     bika.lims.workflow.AfterTransitionEventHandler
     :param obj: Analysis Request affected by the transition
     :type obj: AnalysisRequest
     """
-    sample = obj.getSample()
+    sample = analysis_request.getSample()
     if not sample:
         return
 
     if getCurrentState(sample) != 'rejected':
         doActionFor(sample, 'reject')
-        reasons = obj.getRejectionReasons()
+        reasons = analysis_request.getRejectionReasons()
         sample.setRejectionReasons(reasons)
 
     # Deactivate all analyses from this Analysis Request
-    ans = obj.getAnalyses(full_objects=True)
+    ans = analysis_request.getAnalyses(full_objects=True)
     for analysis in ans:
         doActionFor(analysis, 'reject')
 
-    if obj.bika_setup.getNotifyOnRejection():
+    if analysis_request.bika_setup.getNotifyOnRejection():
         # Notify the Client about the Rejection.
         from bika.lims.utils.analysisrequest import notify_rejection
-        notify_rejection(obj)
+        notify_rejection(analysis_request)
+
+
+def after_dispose(analysis_request):
+    """Method triggered after a 'dispose' transition for the Analysis Request
+    passed in is performed. Tries to perform the same transition to the Sample
+    associated to the analysis request passed in.
+
+    This function is called automatically by
+    bika.lims.workflow.AfterTransitionEventHandler
+
+    :param analysis_request: Analysis Request affected by the transition
+    :type analysis_request: AnalysisRequest
+    """
+    _promote_transition(analysis_request, 'dispose')
+
+
+def after_expire(analysis_request):
+    """Method triggered after a 'expire' transition for the Analysis Request
+    passed in is performed. Tries to perform the same transition to the Sample
+    associated to the analysis request passed in.
+
+    This function is called automatically by
+    bika.lims.workflow.AfterTransitionEventHandler
+
+    :param analysis_request: Analysis Request affected by the transition
+    :type analysis_request: AnalysisRequest
+    """
+    _promote_transition(analysis_request, 'expire')
+
+
+def after_sample_prep(analysis_request):
+    """Method triggered after a 'sample_prep' transition for the Analysis
+    Request passed in is performed. Tries to perform the same transition to the
+    Sample associated to the analysis request passed in.
+
+    This function is called automatically by
+    bika.lims.workflow.AfterTransitionEventHandler
+
+    :param analysis_request: Analysis Request affected by the transition
+    :type analysis_request: AnalysisRequest
+    """
+    _promote_transition(analysis_request, 'sample_prep')
+
+
+def after_sample_prep_complete(analysis_request):
+    """Method triggered after a 'sample_prep' transition for the Analysis
+    Request passed in is performed. Tries to perform the same transition to the
+    Sample associated to the analysis request passed in.
+
+    This function is called automatically by
+    bika.lims.workflow.AfterTransitionEventHandler
+
+    :param analysis_request: Analysis Request affected by the transition
+    :type analysis_request: AnalysisRequest
+    """
+    _promote_transition(analysis_request, 'sample_prep_complete')
 
 
 def after_attach(obj):
